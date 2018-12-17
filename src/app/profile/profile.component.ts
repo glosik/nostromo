6d20f9a4166a16d2bf8ce61e9c13e21fff020daa
Iuri_Strelet <iuri_strelet@Iuri-Strelets-MacBook-Pro.local>
2018-12-17 15:52:36 -0500

    01

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+    profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
