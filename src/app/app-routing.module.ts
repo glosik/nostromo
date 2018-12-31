3edadff3d329ec0213b0efff37e7131e7757ef07
Iuri_Strelet <iuri_strelet@Iuri-Strelets-MacBook-Pro.local>
2018-12-31 09:17:57 -0500

    01. initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/*The Angular Router is an optional service that presents
+a particular component view for a given URL.
+It is not part of the Angular core.*/
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { InteractionsComponent } from './modules/interactions/interactions.component';
+import { ReactiveProgrammingComponent } from './modules/reactive-programming/reactive-programming.component';
+import { RecipeLandingComponent } from './modules/recipes/recipe-landing/recipe-landing.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: 'profile', component: ProfileComponent },
+  { path: 'interactions', component: InteractionsComponent },
+  { path: 'rxjs', component: ReactiveProgrammingComponent },
+  { path: 'recipes', component: RecipeLandingComponent },
+  { path: '404', component: PageNotFoundComponent },
+  /* The application needs a default route to a valid page.
+  The preferred solution is to add a redirect route that translates
+   the initial relative URL ('') to the desired default path (/profile).
+   The browser address bar shows .../heroes as if you'd navigated there directly.*/
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+/*A routed Angular application has one singleton instance of the Router service.
+When the browser's URL changes, that router looks for a corresponding Route
+from which it can determine the component to display.*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,106 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { HackerNewsService } from './services/hacker-news.service';
+import { ScrollListenerService } from './services/scroll-listener.service';
+import { ScrollListenerDirective } from './directives/scroll-listener.directive';
+import { ScrollSpyDirective } from './directives/scroll-spy.directive';
+import { InteractionsComponent } from './modules/interactions/interactions.component';
+import { ParentComponent } from './modules/interactions/parent/parent.component';
+import { SonComponent } from './modules/interactions/son/son.component';
+import { DaughterComponent } from './modules/interactions/daughter/daughter.component';
+import { HovercraftDirective } from './directives/hovercraft.directive';
+import { TextUtils } from './shared/utils/text-utils';
+import { SimpleScrollDirective } from './directives/simple-scroll.directive';
+import { ReactiveStickyHeaderDirective } from './directives/reactive-sticky-header.directive';
+import { AddClassDirective } from './directives/add-class/add-class.directive';
+import { ReactiveProgrammingComponent } from './modules/reactive-programming/reactive-programming.component';
+import { RecipesModule } from './modules/recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    HomePageComponent,
+    AboutComponent,
+    ProjectsComponent,
+    TestimonialsComponent,
+    ScrollListenerDirective,
+    ScrollSpyDirective,
+    InteractionsComponent,
+    ParentComponent,
+    SonComponent,
+    DaughterComponent,
+    HovercraftDirective,
+    SimpleScrollDirective,
+    ReactiveStickyHeaderDirective,
+    AddClassDirective,
+    ReactiveProgrammingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RecipesModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HackerNewsService, ScrollListenerService, TextUtils],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// another way to declare through spread
+
+//const modules = [
+//  CommonModule,
+//  MatFormFieldModule,
+//  MatInputModule,
+//  MatSelectModule,
+//  MatSlideToggleModule,
+//  FormsModule,
+//  ReactiveFormsModule,
+//  MatOptionModule,
+//  MatProgressSpinnerModule,
+//  TranslateModule
+//];
+//
+//const components = [
+//  AvatarComponent,
+//  NotFoundComponent,
+//  PaginationComponent,
+//  SlideToggleComponent
+//];
+//
+//const directives = [
+//  AccButtonDirective,
+//  StickyNavigationDirective,
+//  ScrollSpyDirective
+//];
+//
+//
+//@NgModule({
+//  imports: [
+//    ...modules
+//  ],
+//  declarations: [
+//    ...components,
+//    ...directives
+//  ],
+//  exports: [
+//    ...components,
+//    ...directives,
+//    ...modules
+//  ]
+//})
